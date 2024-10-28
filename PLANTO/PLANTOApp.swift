ad10847aa2ed7d855e1f411b1916342324b3436c
MaramRAlanezi <malanezi24@twq.idserve.net>
2024-10-29 00:01:51 +0300

    Initial Commit

---
+++ PLANTO/PLANTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLANTOApp.swift
+//  PLANTO
+//
+//  Created by Maram Rabeh  on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PLANTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PLANTOTests/PLANTOTests.swift
@@ -0,0 +1,17 @@
+//
+//  PLANTOTests.swift
+//  PLANTOTests
+//
+//  Created by Maram Rabeh  on 26/04/1446 AH.
+//
+
+import Testing
+@testable import PLANTO
+
+struct PLANTOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
